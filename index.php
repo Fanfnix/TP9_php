eed588000fcf5f2e710f2f119a865870b3ab30c5
Fanfnix <valentin@familledenis.net>
2025-03-18 11:59:49 +0100

    Partie 2 (la partie 1 a deja a moité été faites pendant le initial commit mais osef)

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
 <?php require_once("model/pdo.php") ?>
+
+<?php
+
+$resultat = $dbPDO->prepare("SELECT * FROM etudiants");
+$resultat->execute() or die(print_r($resultat->errorInfo()));
+$etudiants = $resultat->fetchAll(PDO::FETCH_CLASS);
+
+$resultat = $dbPDO->prepare("SELECT * FROM classes");
+$resultat->execute() or die(print_r($resultat->errorInfo()));
+$classes = $resultat->fetchAll(PDO::FETCH_CLASS);
+
+$resultat = $dbPDO->prepare("SELECT * FROM professeurs
+    INNER JOIN classes ON professeurs.id_classe = classes.id
+    INNER JOIN matiere ON professeurs.id_matiere = matiere.id
+    ");
+$resultat->execute() or die(print_r($resultat->errorInfo()));
+$professeurs = $resultat->fetchAll(PDO::FETCH_CLASS);
+
+echo "<h2>Liste Etudiants</h2><ul>";
+foreach ($etudiants as $etudiant) echo "<li> $etudiant->nom $etudiant->prenom </li>";
+echo "</ul>";
+
+echo "<h2>Liste Classes</h2><ul>";
+foreach ($classes as $classe) echo "<li> $classe->libelle </li>";
+echo "</ul>";
+
+echo "<h2>Liste Classes</h2><ul>";
+foreach ($professeurs as $professeur) echo "<li> $professeur->nom $professeur->prenom | $professeur->libelle - $professeur->lib</li>";
+echo "</ul>";
+
+?>
--- model/pdo.php
+++ model/pdo.php
@@ -1,15 +1,15 @@
 <?php
 $servername = "localhost";   // Nom du serveur
 $username = "root";   // Nom d'utilisateur de la base de données
-$password = "root";   // Mot de passe de la base de données
-$dbname = "etudiants";   // Nom de la base de données
+$password = "";   // Mot de passe de la base de données
+$dbname = "TP9";   // Nom de la base de données
 
 try {
     $dbPDO = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // Configuration de PDO pour générer des exceptions en cas d'erreur
     $dbPDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion réussie à la base de données"; 
+    echo "Connexion réussie à la base de données<br>"; 
 } catch(PDOException $e) {
-    echo "La connexion a échouée : " . $e->getMessage();
+    echo "La connexion a échouée : " . $e->getMessage() . "<br>";
 }
 ?>
