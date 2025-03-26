29256e7cfc9eeebeccc009cc1cec89a2f4e4221f
Fanfnix <valentin@familledenis.net>
2025-03-26 22:20:30 +0100

    ajout css

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php require_once("model/pdo.php") ?>
 
+<head>
+    <link rel="stylesheet" href="style.css">
+</head>
+
 <?php
 
 $resultat = $dbPDO->prepare("SELECT * FROM etudiants");
@@ -17,27 +21,70 @@ $resultat = $dbPDO->prepare("SELECT * FROM professeurs
 $resultat->execute() or die(print_r($resultat->errorInfo()));
 $professeurs = $resultat->fetchAll(PDO::FETCH_CLASS);
 
-echo "<h2>Liste Etudiants</h2><ul>";
+// Affichage Etudiants
+
+echo "<h2>Liste Etudiants</h2>";
+echo "<table><thead>";
+echo "<tr>
+        <th scope='col'>Prenom</th>
+        <th scope='col'>Nom</th>
+        <th scope='col'>Modifier</th>
+        <th scope='col'>Supprimer</th>
+    </tr>";
+echo "</thead><tbody>";
 foreach ($etudiants as $etudiant) {
     $adresse_modif = "views/modif_etudiant.php?id=".$etudiant->id;
     $adresse_suppr = "views/suppression_etudiant.php?id=".$etudiant->id;
-    echo "<li> $etudiant->nom $etudiant->prenom <a href=$adresse_modif>Modifier</a> <a href=$adresse_suppr>Supprimer</a></li>";
+    echo "<tr>
+        <th scope='col'>$etudiant->nom</th>
+        <th scope='col'>$etudiant->prenom</th>
+        <th scope='col'><a href=$adresse_modif>Modifier</a></th>
+        <th scope='col'><a href=$adresse_suppr>Supprimer</a></th>
+    </tr>";
 }
-echo "</ul>";
+echo "</tbody></table>";
 
-echo "<h2>Liste Classes</h2><ul>";
-foreach ($classes as $classe) echo "<li> $classe->libelle </li>";
-echo "</ul>";
+// Affichage Classes
 
-echo "<h2>Liste Classes</h2><ul>";
-foreach ($professeurs as $professeur) echo "<li> $professeur->nom $professeur->prenom | $professeur->libelle - $professeur->lib</li>";
-echo "</ul>";
+echo "<h2>Liste Classess</h2>";
+echo "<table><thead>";
+echo "<tr>
+        <th scope='col'>Id</th>
+        <th scope='col'>Libelle</th>
+    </tr>";
+echo "</thead><tbody>";
+foreach ($classes as $classe) {
+    echo "<tr>
+        <th scope='col'>$classe->id</th>
+        <th scope='col'>$classe->libelle</th>
+    </tr>";
+}
+echo "</tbody></table>";
 
-$ajout = $dbPDO->prepare("INSERT INTO matiere (lib) VALUES ('Maths')");
-$ajout->execute() or die(print_r($ajout->errorInfo()));
-$result = $ajout->fetchAll(PDO::FETCH_CLASS);
+// Affichage Professeurs
+
+echo "<h2>Liste Professeurs</h2>";
+echo "<table><thead>";
+echo "<tr>
+        <th scope='col'>Prenom</th>
+        <th scope='col'>Nom</th>
+        <th scope='col'>Classe</th>
+        <th scope='col'>Matiere</th>
+    </tr>";
+echo "</thead><tbody>";
+foreach ($professeurs as $professeur) {
+    echo "<tr>
+        <th scope='col'>$professeur->prenom</th>
+        <th scope='col'>$professeur->nom</th>
+        <th scope='col'>$professeur->libelle</th>
+        <th scope='col'>$professeur->lib</th>
+    </tr>";
+}
+echo "</tbody></table>";
 
 ?>
+
+<!-- Formulaire -->
 
 <form action="views/nouvelle_matiere.php" method="post">
     <label for="matiere">Entrez une matière à ajouter</label>
